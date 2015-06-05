c0f3ca5492daac134a9ffdfd4bbbddbc5d85ea3a
Yen-Nan Lin <fo60213@gmail.com>
2015-06-05 19:07:28 +0800

    Add db:seed and rake dev:rebuild

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+puts "Hello World!"
+create_account = User.create([email: 'example@example.com', password: '12345678', password_confirmation: '12345678', name: 'Test Account'])
+create_groups = (1..20).each do |i|
+  Group.create([title: "Group no.#{i}", description: "This is #{i} group", user_id: "1"])
+  (1..30).each do |j|
+    Post.create([group_id: "#{i}", content: "Fake content #{j}", user_id: "1"])
+  end
+end
