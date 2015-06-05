e66262629c31233cac55412ca9fe8543c28421e1
Yen-Nan Lin <fo60213@gmail.com>
2015-06-05 17:16:04 +0800

    Refactory model and view

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,5 @@
 module GroupsHelper
+def render_group_title(title)
+  truncate(title, length: 15)
+end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,5 @@
 module PostsHelper
+  def render_post_content(post)
+    truncate(simple_format(post), length: 200)
+  end
 end
